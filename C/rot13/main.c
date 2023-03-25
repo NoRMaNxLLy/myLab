e390e5f4e135c4ea8ae739e4f8d042a0fad56b79
norman <norman@archT450>
2023-03-25 14:09:29 +0200

    use 'a' instead of 97

--- C/rot13/main.c
+++ C/rot13/main.c
@@ -3,16 +3,16 @@
 #define BUFSIZE 256
 
 /* true if c between a and m*/
-int aTOm(char c) { return 97 <= c && c <= 109? 1: 0; }
+int aTOm(char c) { return 'a' <= c && c <= 'm'? 1: 0; }
 
 /* true if c between n and z*/
-int nTOz(char c) { return 110 <= c && c <= 122? 1: 0; }
+int nTOz(char c) { return 'n' <= c && c <= 'z'? 1: 0; }
 
 /* true if c between A and M*/
-int AtoM(char c) { return 65 <= c && c <= 77? 1: 0; }
+int AtoM(char c) { return 'A' <= c && c <= 'M'? 1: 0; }
 
 /* true if c between N and Z*/
-int NtoZ(char c) { return 78 <= c && c <= 90? 1: 0; }
+int NtoZ(char c) { return 'N' <= c && c <= 'Z'? 1: 0; }
 
 void rot13(char *str)
 {
