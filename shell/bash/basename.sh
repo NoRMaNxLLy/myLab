7d2c017d62e1ac2b1a3553300356978d936ee79b
norman <norman@archT450>
2023-08-17 22:08:31 +0200

    added shell/bash/basename.sh

---
+++ shell/bash/basename.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# mimic the basename(1) command
+# basename /usr/bin/ls           ==> ls
+# basename /usr/bin/foo.bar .bar ==> foo
+basename() {
+    pathname="$1"
+    suffix="$2"
+
+    if [[ $# == 0 || $# > 2 ]]; then
+        echo "Usage: basename <pathname> [suffix]" 1>&2
+        return 1
+    fi
+
+    if [[ -z $pathname ]]; then
+        printf '\n'
+        return
+    fi
+
+    # if pathname is entirely slashes, output a slash
+    if [[ $pathname =~ ^/+$ ]]; then
+        printf '/\n'
+        return
+    fi
+
+    pathname="${pathname%/}"
+    pathname="${pathname##*/}"
+    pathname="${pathname%$suffix}"
+
+    printf '%s\n' "$pathname"
+}
+
+basename "$@"
