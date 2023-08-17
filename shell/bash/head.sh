5c770cb694c72acdd7d8fa8d1a843020a5201b31
norman <norman@archT450>
2023-08-17 15:46:44 +0200

    added shell/bash/head.sh

---
+++ shell/bash/head.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# mimic the head(1) command
+
+# $ head -n<count> [FILE]...
+head() {
+    getopts 'n:' f
+    count="${OPTARG:-10}"
+    shift $((OPTIND - 1))
+    if ((count < 0)) || [[ $count = *[!0-9]* ]]; then
+        echo "invalid number: $count"
+        return 1
+    fi
+
+    # output without header if zero or one argument is passed
+    if (($# <= 1)); then
+        mapfile -t -n $count lines < "${1:-/dev/stdin}"
+        printf '%s\n' "${lines[@]}"
+        return
+    fi
+
+    for f; do
+        mapfile -t -n $count lines < "$f"
+        printf '==> %s <==\n' "$f"
+        printf '%s\n' "${lines[@]}"
+        printf '\n'
+    done
+}
+
+head "$@"
