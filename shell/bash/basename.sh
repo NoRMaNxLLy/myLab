be6efa018e4d59df180727d305dfb3f480a0787b
norman <norman@archT450>
2023-08-18 09:42:43 +0200

    improved shell/bash/basename.sh

--- shell/bash/basename.sh
+++ shell/bash/basename.sh
@@ -4,10 +4,11 @@
 # basename /usr/bin/ls           ==> ls
 # basename /usr/bin/foo.bar .bar ==> foo
 basename() {
+    shopt -s extglob
     pathname="$1"
     suffix="$2"
 
-    if [[ $# == 0 || $# > 2 ]]; then
+    if [[ $# == 0 || $# -gt 2 ]]; then
         echo "Usage: basename <pathname> [suffix]" 1>&2
         return 1
     fi
@@ -23,9 +24,9 @@ basename() {
         return
     fi
 
-    pathname="${pathname%/}"
+    pathname="${pathname%%*(/)}"
     pathname="${pathname##*/}"
-    pathname="${pathname%$suffix}"
+    pathname="${pathname%"$suffix"}"
 
     printf '%s\n' "$pathname"
 }
